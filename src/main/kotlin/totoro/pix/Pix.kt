3da04edd20506d6559b2d3b3028f7a8a7675b434
MoonlightOwl <murky.owl@gmail.com>
2017-06-06 23:05:50 +0200

    add image box

--- src/main/kotlin/totoro/pix/Pix.kt
+++ src/main/kotlin/totoro/pix/Pix.kt
@@ -1,20 +1,27 @@
 package totoro.pix
 
 import javafx.application.Application
-import tornadofx.App
-import tornadofx.View
-import tornadofx.hbox
-import tornadofx.label
+import javafx.scene.control.Alert
+import javafx.scene.image.Image
+import tornadofx.*
 
 fun main(args: Array<String>) {
     Application.launch(Pix::class.java, *args)
 }
 
-class Pix: App (PixView::class) {
-}
+class Pix: App (PixView::class)
 
-class PixView : View() {
-    override val root = hbox {
-        label("Pixin without limits!")
+class PixView: View() {
+    override val root = vbox {
+        button("Open Image...") {
+            action {
+                alert(Alert.AlertType.INFORMATION, "Got ya!", "There is nothing to open here...")
+            }
+        }
+        imageview(Image("/placeholder.png")) {
+            style {
+                padding = box(5.px)
+            }
+        }
     }
 }
