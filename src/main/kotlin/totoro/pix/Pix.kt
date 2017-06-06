93a60712c224601fac3a30af7dbe6e00da452f83
MoonlightOwl <murky.owl@gmail.com>
2017-06-06 22:16:56 +0200

    add tornado

--- src/main/kotlin/totoro/pix/Pix.kt
+++ src/main/kotlin/totoro/pix/Pix.kt
@@ -1,5 +1,20 @@
 package totoro.pix
 
+import javafx.application.Application
+import tornadofx.App
+import tornadofx.View
+import tornadofx.hbox
+import tornadofx.label
+
 fun main(args: Array<String>) {
-    println("Pixin without limits!")
+    Application.launch(Pix::class.java, *args)
+}
+
+class Pix: App (PixView::class) {
+}
+
+class PixView : View() {
+    override val root = hbox {
+        label("Pixin without limits!")
+    }
 }
