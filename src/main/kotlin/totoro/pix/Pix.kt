ccc08163e7998dec4f75a23464a4b5a640b21e55
MoonlightOwl <murky.owl@gmail.com>
2017-06-07 00:23:15 +0200

    allow to load images

--- src/main/kotlin/totoro/pix/Pix.kt
+++ src/main/kotlin/totoro/pix/Pix.kt
@@ -1,8 +1,12 @@
 package totoro.pix
 
 import javafx.application.Application
+import javafx.geometry.Pos
 import javafx.scene.control.Alert
 import javafx.scene.image.Image
+import javafx.scene.layout.BorderStrokeStyle
+import javafx.scene.paint.Color
+import javafx.stage.FileChooser
 import tornadofx.*
 
 fun main(args: Array<String>) {
@@ -12,16 +16,51 @@ fun main(args: Array<String>) {
 class Pix: App (PixView::class)
 
 class PixView: View() {
+    val image = imageview(Image("/placeholder.png")) {
+        fitWidth = 160.0
+        fitHeight = 100.0
+    }
+
     override val root = vbox {
-        button("Open Image...") {
-            action {
-                alert(Alert.AlertType.INFORMATION, "Got ya!", "There is nothing to open here...")
-            }
+        style {
+            padding = box(10.px)
+            spacing = 5.px
         }
-        imageview(Image("/placeholder.png")) {
+        hbox {
             style {
-                padding = box(5.px)
+                spacing = 5.px
+            }
+            button("Open Image...") {
+                action {
+                    val result = chooseFile("Select image file",
+                            arrayOf(FileChooser.ExtensionFilter("Supported image format",
+                                    listOf("*.png", "*.jpg", "*.jpeg", "*.bmp"))))
+                    if (result.isNotEmpty()) {
+                        val file = result.first()
+                        image.image = Image("file:${file.absolutePath}")
+                    }
+                }
+            }
+            button("Export To...") {
+                action {
+                    alert(Alert.AlertType.ERROR, "Unimplemented", "This interface is under construction!")
+                }
             }
         }
+        hbox {
+            style {
+                padding = box(1.px)
+                alignment = Pos.CENTER
+                borderWidth += box(1.px)
+                borderStyle += BorderStrokeStyle.SOLID
+                borderColor += box(Color.DARKGRAY)
+            }
+            children += image
+        }
+    }
+
+    init {
+        title = "Pix Image Converter"
+        primaryStage.isResizable = false
     }
 }
